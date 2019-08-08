ea1c55aa7780569185922f3e7be8f6be7ad80c6c
hiram <hiram@osmium.(none)>
2019-08-07 22:48:57 -0300

    Fix: makefile

---
+++ src/drone_publish_data/Sources/Navdata/navdata.c
@@ -0,0 +1,218 @@
+#include <ardrone_tool/Navdata/ardrone_navdata_client.h>
+
+#include <Navdata/navdata.h>
+#include <stdio.h>
+
+#include <stdlib.h> /*required for MQTT*/
+#include <unistd.h>
+#include <string.h>
+#include "MQTTAsync.h"
+
+#define ADDRESS     "tcp://localhost:1883"
+#define CLIENTID    "DRONE_PUB"
+#define TOPIC       "navdata"
+#define QOS         1
+#define TIMEOUT     10000L
+
+char PAYLOAD[128];
+volatile MQTTAsync_token deliveredtoken;
+int finished = 0;
+
+MQTTAsync client;
+MQTTAsync_connectOptions conn_opts = MQTTAsync_connectOptions_initializer;
+int rc;
+char payload_buffer[50];
+
+/*
+Implementation of itoa
+*/
+
+char* itoa(int val, int base){
+	
+	static char buf[32] = {0};
+	
+	int i = 30;
+	
+	for(; val && i ; --i, val /= base)
+	
+		buf[i] = "0123456789abcdef"[val % base];
+	
+	return &buf[i+1];
+	
+}
+
+void connlost(void *context, char *cause)
+{
+        MQTTAsync client = (MQTTAsync)context;
+        MQTTAsync_connectOptions conn_opts = MQTTAsync_connectOptions_initializer;
+        int rc;
+
+        printf("\nConnection lost\n");
+        printf("     cause: %s\n", cause);
+
+        printf("Reconnecting\n");
+        conn_opts.keepAliveInterval = 20;
+        conn_opts.cleansession = 1;
+        if ((rc = MQTTAsync_connect(client, &conn_opts)) != MQTTASYNC_SUCCESS)
+        {
+                printf("Failed to start connect, return code %d\n", rc);
+                finished = 1;
+        }
+}
+
+
+void onDisconnect(void* context, MQTTAsync_successData* response)
+{
+	printf("Successful disconnection\n");
+	finished = 1;
+}
+
+
+void onSend(void* context, MQTTAsync_successData* response)
+{
+	MQTTAsync client = (MQTTAsync)context;
+	MQTTAsync_disconnectOptions opts = MQTTAsync_disconnectOptions_initializer;
+	int rc;
+
+	printf("Message with token value %d delivery confirmed\n", response->token);
+
+	opts.onSuccess = onDisconnect;
+	opts.context = client;
+
+	if ((rc = MQTTAsync_disconnect(client, &opts)) != MQTTASYNC_SUCCESS)
+	{
+		printf("Failed to start sendMessage, return code %d\n", rc);
+		exit(EXIT_FAILURE);
+	}
+}
+
+
+void onConnectFailure(void* context, MQTTAsync_failureData* response)
+{
+	printf("Connect failed, rc %d\n", response ? response->code : 0);
+	finished = 1;
+}
+
+
+void onConnect(void* context, MQTTAsync_successData* response)
+{
+	MQTTAsync client = (MQTTAsync)context;
+	MQTTAsync_responseOptions opts = MQTTAsync_responseOptions_initializer;
+	MQTTAsync_message pubmsg = MQTTAsync_message_initializer;
+	int rc;
+
+	printf("Successful connection\n");
+
+	opts.onSuccess = onSend;
+	opts.context = client;
+
+	pubmsg.payload = PAYLOAD;
+	pubmsg.payloadlen = (int)strlen(PAYLOAD);
+	pubmsg.qos = QOS;
+	pubmsg.retained = 0;
+	deliveredtoken = 0;
+
+	if ((rc = MQTTAsync_sendMessage(client, TOPIC, &pubmsg, &opts)) != MQTTASYNC_SUCCESS)
+	{
+		printf("Failed to start sendMessage, return code %d\n", rc);
+		exit(EXIT_FAILURE);
+	}
+}
+
+/* Initialization local variables before event loop  */
+inline C_RESULT demo_navdata_client_init( void* data )
+{
+	MQTTAsync_create(&client, ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
+	MQTTAsync_setCallbacks(client, NULL, connlost, NULL, NULL);
+	conn_opts.keepAliveInterval = 20;
+	conn_opts.cleansession = 1;
+	conn_opts.onSuccess = onConnect;
+	conn_opts.onFailure = onConnectFailure;
+	conn_opts.context = client;
+	//conn_opts.username = "grbucbfp";
+	//conn_opts.password = "EjchUbjpJX71";
+
+  return C_OK;
+}
+
+/* Receving navdata during the event loop */
+inline C_RESULT demo_navdata_client_process( const navdata_unpacked_t* const navdata )
+{
+	const navdata_demo_t*nd = &navdata->navdata_demo;
+
+	strcat(PAYLOAD , "Control state :");
+	strcat(PAYLOAD , itoa(nd->ctrl_state , 10));
+
+	if ((rc = MQTTAsync_connect(client, &conn_opts)) != MQTTASYNC_SUCCESS)
+	{
+		printf("Failed to start connect, return code %d\n", rc);
+		exit(EXIT_FAILURE);
+	}
+	printf("Waiting for publication of %s\n on topic %s for client with ClientID: %s\n", PAYLOAD, TOPIC, CLIENTID);
+
+
+/*	printf("Control state : %i\n",nd->ctrl_state);
+	printf("Battery level : %i mV\n",nd->vbat_flying_percentage);
+	printf("Orientation   : [Theta] %4.3f  [Phi] %4.3f  [Psi] %4.3f\n",nd->theta,nd->phi,nd->psi);
+	printf("Altitude      : %i\n",nd->altitude);
+	printf("Speed         : [vX] %4.3f  [vY] %4.3f  [vZPsi] %4.3f\n",nd->theta,nd->phi,nd->psi);
+	printf("\033[8A");
+*/	
+	sleep(5);
+
+  return C_OK;
+}
+
+/* Relinquish the local resources after the event loop exit */
+inline C_RESULT demo_navdata_client_release( void )
+{
+	MQTTAsync_destroy(&client);
+  return C_OK;
+}
+
+/* Registering to navdata client */
+BEGIN_NAVDATA_HANDLER_TABLE
+  NAVDATA_HANDLER_TABLE_ENTRY(demo_navdata_client_init, demo_navdata_client_process, demo_navdata_client_release, NULL)
+END_NAVDATA_HANDLER_TABLE
+
+/*
+#include <ardrone_tool/Navdata/ardrone_navdata_client.h>
+
+#include <Navdata/navdata.h>
+#include <stdio.h>
+
+/* Initialization local variables before event loop  *
+inline C_RESULT demo_navdata_client_init( void* data )
+{
+  return C_OK;
+}
+
+/* Receving navdata during the event loop *
+inline C_RESULT demo_navdata_client_process( const navdata_unpacked_t* const navdata )
+{
+	const navdata_demo_t*nd = &navdata->navdata_demo;
+
+	printf("=====================\nNavdata for flight demonstrations =====================\n\n");
+
+	printf("Control state : %i\n",nd->ctrl_state);
+	printf("Battery level : %i mV\n",nd->vbat_flying_percentage);
+	printf("Orientation   : [Theta] %4.3f  [Phi] %4.3f  [Psi] %4.3f\n",nd->theta,nd->phi,nd->psi);
+	printf("Altitude      : %i\n",nd->altitude);
+	printf("Speed         : [vX] %4.3f  [vY] %4.3f  [vZPsi] %4.3f\n",nd->theta,nd->phi,nd->psi);
+
+	printf("\033[8A");
+
+  return C_OK;
+}
+
+/* Relinquish the local resources after the event loop exit *
+inline C_RESULT demo_navdata_client_release( void )
+{
+  return C_OK;
+}
+
+/* Registering to navdata client *
+BEGIN_NAVDATA_HANDLER_TABLE
+  NAVDATA_HANDLER_TABLE_ENTRY(demo_navdata_client_init, demo_navdata_client_process, demo_navdata_client_release, NULL)
+END_NAVDATA_HANDLER_TABLE
+*/
